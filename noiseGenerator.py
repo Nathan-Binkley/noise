4d3189982dc86af53a5d542401e705531e6ff653
Nathan-Binkley <nbink2@gmail.com>
2020-05-02 17:50:10 -0400

    Initial commit

--- noiseGenerator.py
+++ noiseGenerator.py
@@ -1,26 +1,27 @@
 import pygame
 import random
+import noise
 #Basic premise is to create "noise" More specifically Perlin noice using Pygame to visualize this noise
 
 def createNoise(x = 50, y = 50):
     master_points = []
     startPoint = 0
     for i in range(x):
-        startPoint = random.randint()
+        startPoint = random.randint(0,10000)
         rowPoints = []
         for i in range(y):
             choice = random.randint(0,2) #grow (2) or shrink (0) or stay equal (1)
             rowPoints.append(startPoint) # all 3 RGB for this particular pixel
             if choice == 2:
-                startPoint += 1
+                startPoint += random.randint(0,30)
             elif choice == 0:
-                startPoint -= 1
+                startPoint -= random.randint(0,30)
             else:
                 pass
         master_points.append(rowPoints) # Creates a 2D array
     return master_points
 
-def getMaxMin2D(Array):
+def getMaxMin2D(Array): #Helpful to get a normalized scale of the noise
     maximum = Array[0][0]
     minimum = Array[0][0]
     for i in Array:
@@ -32,6 +33,8 @@ def getMaxMin2D(Array):
     return [minimum, maximum]
 
 
-def displayNoice():
-    
+def displayNoise():
+
     pass
+
+print(createNoise())

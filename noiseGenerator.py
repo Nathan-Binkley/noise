b69dace8d5879155d17876cc81a8df3a3c60f89c
Nathan-Binkley <nbink2@gmail.com>
2020-05-09 21:00:10 -0400

    Reorganizing

--- noiseGenerator.py
+++ noiseGenerator.py
@@ -7,33 +7,38 @@ import random
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
-WIDTH = 50 # SET LARGER IF YOU WANT YOUR LAPTOP TO DIE -- or if running on more beefy PC
-HEIGHT = 50 # 800-1000 is optimal stress test
-RESOLUTION = 5
-BW = False # Set to true if you want just black and white noise
+WIDTH = 500 # SET LARGER IF YOU WANT YOUR LAPTOP TO DIE -- or if running on more beefy PC
+HEIGHT = 500 # 800-1000 is optimal stress test
+RESOLUTION = 1
+BW = True # Set to true if you want just black and white noise
+
+noiseType = 0 #1 for regular Noise, non-1 for Perlin Noise
 
 
-def createNoise():
+def createNoise(): # Creates matrix of 1s and 0s
     master_points = []
     for i in range(HEIGHT):
         x_points = []
         for j in range(WIDTH):
-            x_points.append(random.randint(0,255))
+            x_points.append(random.randint(0,1))
         master_points.append(x_points)
+
     return master_points
 
-def createPNoise(x = WIDTH, y = HEIGHT):
+def createPNoise(x = WIDTH, y = HEIGHT): # Creates ratios between 0 and 1
+    
     master_points = []
     startPoint = 0
     above = 0
     addSub = 0
+
     for i in range(x):
         try:
             startAbove = master_points[i-1][0]
         except:
-            startAbove = random.randint(500,10000)
+            startAbove = random.randint(50000,100000)
         addSub = random.randint(0,2)
-        if addSub == 0: #subtract
+        if addSub == 0 and startAbove > 0: #subtract
             startPoint = startAbove - random.randint(3,30)
         elif addSub == 2: # add
             startPoint = startAbove + random.randint(3,30)
@@ -43,21 +48,42 @@ def createPNoise(x = WIDTH, y = HEIGHT):
         rowPoints = []
 
         for j in range(y):
-            choice = random.randint(0,2) #grow (2) or shrink (0) or stay equal (1)
-            rowPoints.append(startPoint % 255) # all 3 RGB for this particular pixel
             try:
-                above = master_points[i-1][y] 
-            except:
+                above = master_points[i-1][j] 
+                # print("ABOVE CURRENT POINT: " + str(above))
+                # print("CURRENT POINT: " + str(startPoint))
+            except Exception as e:
+                # print(e)
                 above = 0
-            if choice == 2: #ADD to
+
+            choice = random.randint(0,2) #grow (2) or shrink (0) or stay equal (1)
+            rowPoints.append(startPoint) # all 3 RGB for this particular pixel
+
+            if choice == 2: #Add to
+
                 startPoint += random.randint(3,30)
-                
+            
             elif choice == 0: #Subtract from
+
                 startPoint -= random.randint(3,30)
+
             else:
                 pass
+
         master_points.append(rowPoints) # Creates a 2D array
+
+    scalar = getMinMax2D(master_points)
+    minPoints = scalar[0]
+    maxPoints = scalar[1]
+    print(scalar)
     
+    for i in range(len(master_points)): #ROW
+        for j in range(len(master_points[i])): #COL
+            
+            master_points[i][j] = master_points[i][j]/maxPoints
+
+            #print(master_points[i][j])
+
     return master_points
 
 def getMinMax2D(Array): #Helpful to get a normalized scale of the noise
@@ -74,17 +100,17 @@ def getMinMax2D(Array): #Helpful to get a normalized scale of the noise
 
 def displayNoise(Noise):
     pygame.init()
-    window = pygame.display.set_mode((WIDTH * RESOLUTION, HEIGHT * RESOLUTION)) 
+    window = pygame.display.set_mode((WIDTH * RESOLUTION , HEIGHT * RESOLUTION )) 
 
-    y = 0
-    x = 0
 
     fpsClock = pygame.time.Clock()  
 
     globalMin = getMinMax2D(Noise)[0]
     globalMax = getMinMax2D(Noise)[1]
-
+    # print(Noise)
     while True:
+        y = 0
+        x = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -94,30 +120,34 @@ def displayNoise(Noise):
             x = 0
             for pixel in row:
                 if not BW:
-                    window.set_at((x, y), (pixel,pixel,pixel))
-                    pygame.draw.rect(window,(pixel,pixel,pixel),(x,y,RESOLUTION,RESOLUTION))
+                    #window.set_at((x, y), (int(pixel*255), int(pixel*255), int(pixel*255)))
+                    try:
+                        pygame.draw.rect(window,BLACK,(x, y, RESOLUTION, RESOLUTION))
+                    except Exception as e:
+                        pass
+                        # print(e)
+                        # print(pixel)
                 else:
-                    if pixel > 255//2:
-                        pygame.draw.rect(window,BLACK,(x,y,RESOLUTION,RESOLUTION))
+                    if noiseType == 1:
+                        pygame.draw.rect(window,(int(pixel*255), int(pixel*255), int(pixel*255)),(x, y, RESOLUTION, RESOLUTION))
                     else:
-                        pygame.draw.rect(window,WHITE,(x,y,RESOLUTION,RESOLUTION))
+                        if pixel > 255//2:
+                            pygame.draw.rect(window,BLACK,(x, y, RESOLUTION, RESOLUTION))
+                        else:
+                            pygame.draw.rect(window,WHITE,(x, y, RESOLUTION, RESOLUTION))
                 x += RESOLUTION
             y += RESOLUTION
-        y = 0
+        
         pygame.display.flip()
         fpsClock.tick(30)
 
 global_min = []
 global_max = []
 
-x = createPNoise()
-minMax = getMinMax2D(x)
-Loc_Min = minMax[0]
-Loc_Max = minMax[1]
-# global_min.append(Loc_Min)
-# global_max.append(Loc_Max)
-displayNoise(x)
+noise = 0
+if noiseType == 1:
+    noise = createNoise()
+else:
+    noise = createPNoise()
 
-
-#print(x)
-#print("MIN: " + str(min(global_min)) + " MAX: " + str(max(global_max)))
+displayNoise(noise)
